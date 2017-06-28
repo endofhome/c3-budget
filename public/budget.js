475a579a6088a4adf11b9144b60315365872ae59
Tom Barnes <ruralmyth@gmail.com>
2017-06-28 23:35:55 +0100

    budget.js now handles two months and add annual overview chart

--- public/budget.js
+++ public/budget.js
@@ -1,10 +1,12 @@
-var jsonFile = "./data.json";
+var jsonFiles = ["./data-2017-05.json", "./data-2017-06.json"];
+var numberOfMonths = jsonFiles.length;
 var budget;
+var budgetLastMonth;
 
-var loadFileSync = function (callback) {
+var loadFileSync = function(file, callback) {
     var xhr = new XMLHttpRequest();
     xhr.overrideMimeType("application/json");
-    xhr.open('GET', jsonFile, false);
+    xhr.open('GET', file, false);
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             callback(xhr.responseText);
@@ -13,9 +15,13 @@ var loadFileSync = function (callback) {
     xhr.send(null);
 };
 
-var loadJsonFromFS = function () {
-    loadFileSync(function (response) {
+var loadJsonFromFS = function() {
+    loadFileSync(jsonFiles[1], function (response) {
         budget = JSON.parse(response);
+    });
+
+    loadFileSync(jsonFiles[0], function (response) {
+        budgetLastMonth = JSON.parse(response);
     });
 };
 
@@ -105,6 +111,34 @@ var amalgamatedJson = function(array, name) {
     return data
 };
 
+var combineMonthData = function (categories) {
+    var result = [];
+    categories[0].forEach(function (category) {
+        result.push({
+            "title": category["title"],
+            "data": category["data"]
+        });
+        var lastEntry = result[result.length - 1];
+        var titleToFind = lastEntry["title"];
+        var nameToFind;
+        var findTitle = function (category) {
+            return category.title === titleToFind
+        };
+
+        var findName = function (dataItem) {
+            return dataItem.name === nameToFind
+        };
+        var correspondingCategory = categories[1].find(findTitle);
+        lastEntry["data"].forEach(function (dataItem) {
+            nameToFind = dataItem["name"];
+            var correspondingItem = correspondingCategory["data"].find(findName);
+            dataItem["monthly_budget"] += correspondingItem["monthly_budget"];
+            dataItem["actual"] += correspondingItem["actual"]
+        });
+    });
+    return result;
+};
+
 loadJsonFromFS();
 var inYourHome = new Category("In your home", "#in-your-home", 1000);
 var insurance = new Category("Insurance", "#insurance");
@@ -118,13 +152,16 @@ var clothes = new Category("Clothes", "#clothes");
 var bigOneOffs = new Category("Big one offs", "#big-one-offs");
 var oddsAndSods = new Category("Odds and sods", "#odds-and-sods");
 var monthOverview = amalgamatedJson(budget.categories);
+var lastMonthOverview = amalgamatedJson(budgetLastMonth.categories);
 var monthlyTotal = [amalgamatedJson(monthOverview, budget.month + " " + budget.year)[0]];
+var lastMonthlyTotal = [amalgamatedJson(lastMonthOverview, budgetLastMonth.month + " " + budgetLastMonth.year)[0]];
 
 var _annualTotal = function() {
     var result = {};
     var monthlyBudget = monthlyTotal[0].monthly_budget;
-    result.actual = monthlyTotal[0].actual;
-    result.monthly_budget = monthlyBudget * 12;
+    result.actual = monthlyTotal[0].actual + lastMonthlyTotal[0].actual;
+    result.monthly_budget = monthlyBudget * numberOfMonths;
+    result.annual_budget = monthlyBudget * 12;
     result.name = "2017-2018";
     return [result];
 };
@@ -207,6 +244,43 @@ var yearTotal = c3.generate({
         json: _annualTotal(),
         keys: {
             x: 'name',
+            value: ['annual_budget', 'monthly_budget', 'actual']
+        },
+        type: 'bar',
+        color: function (color, d) {
+            if (!d.id && d === 'monthly_budget') {
+                return '#cce5ff'
+            } else if (d.id === 'monthly_budget') {
+                return '#cce5ff'
+            } else if (d.id === 'annual_budget') {
+                return 'gray'
+            } else if (d.id) {
+                var monthlyBudget = _annualTotal()[d.index].monthly_budget;
+                return d.id === 'actual' && d.value > (monthlyBudget * numberOfMonths) ? '#ff0000' : '#33cc33';
+            }
+        }
+    },
+    bar: {
+        width: {
+            ratio: 0.5
+        }
+    },
+    axis: {
+        rotated: true,
+        x: {
+            type: 'category'
+        }
+    }
+});
+
+var yearOverview = c3.generate({
+    bindto: '#year-overview',
+    data: {
+        labels: true,
+        x : 'x',
+        json: amalgamatedJson(combineMonthData([budget.categories, budgetLastMonth.categories])),
+        keys: {
+            x: 'name',
             value: ['monthly_budget', 'actual']
         },
         type: 'bar',
@@ -216,7 +290,7 @@ var yearTotal = c3.generate({
             } else if (d.id === 'monthly_budget') {
                 return '#cce5ff'
             } else if (d.id) {
-                var monthlyBudget = _annualTotal()[d.index].monthly_budget;
+                var monthlyBudget = amalgamatedJson(budget.categories)[d.index].monthly_budget;
                 return d.id === 'actual' && d.value > monthlyBudget ? '#ff0000' : '#33cc33';
             }
         }
