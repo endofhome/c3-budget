9bede2118863a32ec1c73341e3416efb662270a7
Tom Barnes <ruralmyth@gmail.com>
2017-08-13 21:57:34 +0100

    some ES2015-ification

--- public/budget.js
+++ public/budget.js
@@ -1,10 +1,10 @@
-var jsonFiles = ["./budgets/budget-2017-2018.json", "./actual-expenditure/actual-expenditure-2017-05.json", "./actual-expenditure/actual-expenditure-2017-06.json"];
-var numberOfMonths = jsonFiles.length;
-var budget;
-var budgetLastMonth;
+const jsonFiles = ["./budgets/budget-2017-2018.json", "./actual-expenditure/actual-expenditure-2017-05.json", "./actual-expenditure/actual-expenditure-2017-06.json"];
+const numberOfMonths = jsonFiles.length - 1;
+let budget;
+let budgetLastMonth;
 
-var loadFileSync = function(file, callback) {
-    var xhr = new XMLHttpRequest();
+const loadFileSync = function(file, callback) {
+    const xhr = new XMLHttpRequest();
     xhr.overrideMimeType("application/json");
     xhr.open('GET', file, false);
     xhr.onreadystatechange = function () {
@@ -15,7 +15,19 @@ var loadFileSync = function(file, callback) {
     xhr.send(null);
 };
 
-var loadJsonFromFS = function() {
+const _addMonthlyBudgetTotals = function(monthData, monthlyBudgetData) {
+    monthData["categories"].forEach(category => {
+        const budgetDataForCategory = monthlyBudgetData["categories"]
+            .filter(cat => category.title === cat.title)[0];
+        category["data"].forEach(dataItem => {
+            const budgetDataForDataItem = budgetDataForCategory["data"]
+                .filter(item => item["name"] === dataItem["name"])[0];
+            dataItem["monthly_budget"] = budgetDataForDataItem["monthly_budget"];
+        });
+    })
+};
+
+const loadJsonFromFS = function() {
     let monthlyBudgetTotals;
 
     loadFileSync(jsonFiles[0], function (response) {
@@ -33,18 +45,13 @@ var loadJsonFromFS = function() {
     });
 };
 
-var _addMonthlyBudgetTotals = function(monthData, monthlyBudgetData) {
-    monthData["categories"].forEach(category => {
-        var budgetDataForCategory = monthlyBudgetData["categories"]
-            .filter(cat => category.title === cat.title)[0];
-        category["data"].forEach(dataItem => {
-            var budgetDataForDataItem = budgetDataForCategory["data"]
-                .filter(item => item["name"] === dataItem["name"])[0];
-            dataItem["monthly_budget"] = budgetDataForDataItem["monthly_budget"];
-        });
-    })
-};
 
+const _categoryData = function(title) {
+    const matchingCategory = budget.categories.filter(function (item) {
+        return item.title === title;
+    });
+    return matchingCategory[0].data;
+};
 function Category(name, binding, height) {
     this.name = name;
     this.binding = binding;
@@ -66,11 +73,11 @@ function Category(name, binding, height) {
                 } else if (d.id === 'monthly_budget') {
                     return '#cce5ff'
                 } else if (d.id) {
-                    var estimated = _categoryData(name)[d.index].estimated;
+                    const estimated = _categoryData(name)[d.index].estimated;
                     if (estimated) {
                         return '990099'
                     }
-                    var monthlyBudget = _categoryData(name)[d.index].monthly_budget;
+                    const monthlyBudget = _categoryData(name)[d.index].monthly_budget;
                     return d.id === 'actual' && d.value > monthlyBudget ? '#ff0000' : '#33cc33';
                 }
             }
@@ -97,27 +104,21 @@ function Category(name, binding, height) {
     c3.generate(this.c3Data);
 }
 
-var _categoryData = function(title) {
-    var matchingCategory = budget.categories.filter(function(item) {
-        return item.title === title;
-    });
-    return matchingCategory[0].data;
-};
 
-var amalgamatedJson = function(array, name) {
-    var data = [];
-    array.forEach(function(item) {
-        var budgetTotal = 0;
-        var actualTotal = 0;
-        var dataElement = item.data;
+const amalgamatedJson = function (array, name) {
+    const data = [];
+    array.forEach(function (item) {
+        let budgetTotal = 0;
+        let actualTotal = 0;
+        let dataElement = item.data;
         if (!item.data) {
             dataElement = array
         }
-        dataElement.forEach(function(monthItem) {
+        dataElement.forEach(function (monthItem) {
             budgetTotal = budgetTotal + monthItem.monthly_budget;
             actualTotal = actualTotal + monthItem.actual;
         });
-        var object = {};
+        const object = {};
         object.name = item.title;
         if (!item.title) {
             object.name = name;
@@ -131,27 +132,27 @@ var amalgamatedJson = function(array, name) {
     return data
 };
 
-var combineMonthData = function (categories) {
-    var result = [];
-    categories[0].forEach(function (category) {
+const combineMonthData = function(categories) {
+    const result = [];
+    categories[0].forEach(function(category) {
         result.push({
             "title": category["title"],
             "data": category["data"]
         });
-        var lastEntry = result[result.length - 1];
-        var titleToFind = lastEntry["title"];
-        var nameToFind;
-        var findTitle = function (category) {
+        const lastEntry = result[result.length - 1];
+        const titleToFind = lastEntry["title"];
+        let nameToFind;
+        const findTitle = function (category) {
             return category.title === titleToFind
         };
 
-        var findName = function (dataItem) {
+        const findName = function (dataItem) {
             return dataItem.name === nameToFind
         };
-        var correspondingCategory = categories[1].find(findTitle);
-        lastEntry["data"].forEach(function (dataItem) {
+        const correspondingCategory = categories[1].find(findTitle);
+        lastEntry["data"].forEach(function(dataItem) {
             nameToFind = dataItem["name"];
-            var correspondingItem = correspondingCategory["data"].find(findName);
+            const correspondingItem = correspondingCategory["data"].find(findName);
             dataItem["monthly_budget"] += correspondingItem["monthly_budget"];
             dataItem["actual"] += correspondingItem["actual"]
         });
@@ -160,25 +161,25 @@ var combineMonthData = function (categories) {
 };
 
 loadJsonFromFS();
-var inYourHome = new Category("In your home", "#in-your-home", 1000);
-var insurance = new Category("Insurance", "#insurance");
-var eatsAndDrinks = new Category("Eats and drinks", "#eats-and-drinks");
-var motoringAndPublicTransport = new Category("Motoring and public transport", "#motoring-and-public-transport");
-var savingsAndInvestments = new Category("Savings and investments", "#savings-and-investments");
-var family = new Category("Family", "#family");
-var fun = new Category("Fun", "#fun");
-var healthAndBeauty = new Category("Health and beauty", "#health-and-beauty");
-var clothes = new Category("Clothes", "#clothes");
-var bigOneOffs = new Category("Big one offs", "#big-one-offs");
-var oddsAndSods = new Category("Odds and sods", "#odds-and-sods");
-var monthOverview = amalgamatedJson(budget.categories);
-var lastMonthOverview = amalgamatedJson(budgetLastMonth.categories);
-var monthlyTotal = [amalgamatedJson(monthOverview, budget.month + " " + budget.year)[0]];
-var lastMonthlyTotal = [amalgamatedJson(lastMonthOverview, budgetLastMonth.month + " " + budgetLastMonth.year)[0]];
+const inYourHome = new Category("In your home", "#in-your-home", 1000);
+const insurance = new Category("Insurance", "#insurance");
+const eatsAndDrinks = new Category("Eats and drinks", "#eats-and-drinks");
+const motoringAndPublicTransport = new Category("Motoring and public transport", "#motoring-and-public-transport");
+const savingsAndInvestments = new Category("Savings and investments", "#savings-and-investments");
+const family = new Category("Family", "#family");
+const fun = new Category("Fun", "#fun");
+const healthAndBeauty = new Category("Health and beauty", "#health-and-beauty");
+const clothes = new Category("Clothes", "#clothes");
+const bigOneOffs = new Category("Big one offs", "#big-one-offs");
+const oddsAndSods = new Category("Odds and sods", "#odds-and-sods");
+const monthOverview = amalgamatedJson(budget.categories);
+const lastMonthOverview = amalgamatedJson(budgetLastMonth.categories);
+const monthlyTotal = [amalgamatedJson(monthOverview, budget.month + " " + budget.year)[0]];
+const lastMonthlyTotal = [amalgamatedJson(lastMonthOverview, budgetLastMonth.month + " " + budgetLastMonth.year)[0]];
 
-var _annualTotal = function() {
-    var result = {};
-    var monthlyBudget = monthlyTotal[0].monthly_budget;
+const _annualTotal = function () {
+    const result = {};
+    const monthlyBudget = monthlyTotal[0].monthly_budget;
     result.actual = monthlyTotal[0].actual + lastMonthlyTotal[0].actual;
     result.monthly_budget = monthlyBudget * numberOfMonths;
     result.annual_budget = monthlyBudget * 12;
@@ -186,11 +187,11 @@ var _annualTotal = function() {
     return [result];
 };
 
-var monthTotal = c3.generate({
+const monthTotal = c3.generate({
     bindto: '#month-total',
     data: {
         labels: true,
-        x : 'x',
+        x: 'x',
         json: monthlyTotal,
         keys: {
             x: 'name',
@@ -221,11 +222,11 @@ var monthTotal = c3.generate({
     }
 });
 
-var monthlyOverview = c3.generate({
+const monthlyOverview = c3.generate({
     bindto: '#month-overview',
     data: {
         labels: true,
-        x : 'x',
+        x: 'x',
         json: amalgamatedJson(budget.categories),
         keys: {
             x: 'name',
@@ -256,11 +257,11 @@ var monthlyOverview = c3.generate({
     }
 });
 
-var yearTotal = c3.generate({
+const yearTotal = c3.generate({
     bindto: '#year-total',
     data: {
         labels: true,
-        x : 'x',
+        x: 'x',
         json: _annualTotal(),
         keys: {
             x: 'name',
@@ -293,11 +294,11 @@ var yearTotal = c3.generate({
     }
 });
 
-var yearOverview = c3.generate({
+const yearOverview = c3.generate({
     bindto: '#year-overview',
     data: {
         labels: true,
-        x : 'x',
+        x: 'x',
         json: amalgamatedJson(combineMonthData([budget.categories, budgetLastMonth.categories])),
         keys: {
             x: 'name',
